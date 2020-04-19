f4e7132a404d65d12a0eac503ea281ea0ac21857
Vishesh Singh <VisheshSingh16@gmail.com>
2020-04-19 18:06:13 -0500

    setup + folder structure with bootstrap, styled-components, router

---
+++ src/components/pages/Home.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className='container'>
+      <h1 className='display-5 text-uppercae text-center py-5'>
+        Welcome to JavaScript 2020 conference
+      </h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Id ducimus
+        dolor, aliquam mollitia, doloribus dolorem nam accusamus hic, nobis
+        corporis minima omnis. Enim provident possimus ab? Accusantium corrupti
+        sequi numquam. Placeat laudantium qui ducimus eaque perferendis
+        provident animi quo saepe. Nostrum ipsa sunt aut deleniti corporis totam
+        reprehenderit adipisci ut neque voluptas alias, excepturi, iusto minima,
+        assumenda beatae consequatur laudantium?
+      </p>
+      <div className='form-book'></div>
+    </div>
+  );
+}
+
+export default Home;
