6e4a48f5e792d8ccbd789dd7ab45c1d5b61787a5
Vishesh Singh <VisheshSingh16@gmail.com>
2020-04-19 20:05:05 -0500

    Footer 🦶

---
+++ src/components/layout/Footer.js
@@ -0,0 +1,99 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+function Footer() {
+  return (
+    <FooterContainer>
+      <div className='container-fluid'>
+        <div className='row pt-5'>
+          {/* Column 1 */}
+          <div className='col-sm-3 col-xs-12 d-flex'>
+            <ul className='list-inline mx-auto justify-content-center'>
+              <Link to='/'>
+                <li>Lorem ipsum</li>
+              </Link>
+              <Link to='/'>
+                <li>Lorem ipsum</li>
+              </Link>
+              <Link to='/'>
+                <li>Lorem ipsum</li>
+              </Link>
+            </ul>
+          </div>
+          {/* Column 2 */}
+          <div className='col-sm-3 col-xs-12 d-flex'>
+            <ul className='list-inline mx-auto justify-content-center'>
+              <Link to='/'>
+                <li>star wars ipsum</li>
+              </Link>
+              <Link to='/'>
+                <li>star wars ipsum</li>
+              </Link>
+              <Link to='/'>
+                <li>star wars ipsum</li>
+              </Link>
+            </ul>
+          </div>
+          {/* Column 3 */}
+          <div className='col-sm-3 col-xs-12 d-flex'>
+            <ul className='list-inline mx-auto justify-content-center'>
+              <Link to='/'>
+                <li>marvel ipsum</li>
+              </Link>
+              <Link to='/'>
+                <li>marvel ipsum</li>
+              </Link>
+              <Link to='/'>
+                <li>marvel ipsum</li>
+              </Link>
+            </ul>
+          </div>
+          {/* Column 4 */}
+          <div className='col-sm-3 col-xs-12 d-flex'>
+            <ul className='list-inline mx-auto justify-content-center'>
+              <Link to='/'>
+                <li>ninja ipsum</li>
+              </Link>
+              <Link to='/'>
+                <li>ninja ipsum</li>
+              </Link>
+              <Link to='/'>
+                <li>ninja ipsum</li>
+              </Link>
+            </ul>
+          </div>
+          <div className='pt-3 pb-2 pl-5 copy-right text-center'>
+            &nbsp; 👑 {new Date().getFullYear()}&nbsp; &copy; All Rights
+            Reserved.
+          </div>
+        </div>
+      </div>
+    </FooterContainer>
+  );
+}
+
+export default Footer;
+
+const FooterContainer = styled.footer`
+  background: var(--deep-dark);
+  border-top: 1px solid var(--yummy-burgundy);
+
+  ul li {
+    list-style: none;
+  }
+
+  a {
+    color: var(--dark);
+    text-decoration: none;
+    &:hover {
+      color: var(--yummy-burgundy);
+      text-decoration: none;
+    }
+  }
+
+  .copy-right {
+    width: 100%;
+    color: var(--yummy-burgundy);
+  }
+`;
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,69 +1,73 @@
 import React from 'react';
 import styled from 'styled-components';
 import Navbar from '../layout/Navbar';
+import Footer from '../layout/Footer';
 
 function Home() {
   return (
-    <MainContainer>
-      <div className='home-main-container'>
-        <Navbar />
-        <div className='container'>
-          <h1 className='display-5 text-uppercase text-center py-5'>
-            Welcome to JavaScript 2020 conference
-          </h1>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Id ducimus
-            dolor, aliquam mollitia, doloribus dolorem nam accusamus hic, nobis
-            corporis minima omnis. Enim provident possimus ab? Accusantium
-            corrupti sequi numquam. Placeat laudantium qui ducimus eaque
-            perferendis provident animi quo saepe. Nostrum ipsa sunt aut
-            deleniti corporis totam reprehenderit adipisci ut neque voluptas
-            alias, excepturi, iusto minima, assumenda beatae consequatur
-            laudantium?
-          </p>
-          <div className='form-box'>
-            <h3 className='display-6 text-uppercase text-center py-3'>
-              register and reserve your seat
-            </h3>
-            <form>
-              <div className='form-row mr-auto'>
-                <div className='col-md-6 mb-4'>
-                  <input
-                    type='text'
-                    className='form-control'
-                    placeholder='First name'
-                  />
+    <>
+      <MainContainer>
+        <div className='home-main-container'>
+          <Navbar />
+          <div className='container'>
+            <h1 className='display-5 text-uppercase text-center py-5'>
+              Welcome to JavaScript 2020 conference
+            </h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Id
+              ducimus dolor, aliquam mollitia, doloribus dolorem nam accusamus
+              hic, nobis corporis minima omnis. Enim provident possimus ab?
+              Accusantium corrupti sequi numquam. Placeat laudantium qui ducimus
+              eaque perferendis provident animi quo saepe. Nostrum ipsa sunt aut
+              deleniti corporis totam reprehenderit adipisci ut neque voluptas
+              alias, excepturi, iusto minima, assumenda beatae consequatur
+              laudantium?
+            </p>
+            <div className='form-box'>
+              <h3 className='display-6 text-uppercase text-center py-3'>
+                register and reserve your seat
+              </h3>
+              <form>
+                <div className='form-row mr-auto'>
+                  <div className='col-md-6 mb-4'>
+                    <input
+                      type='text'
+                      className='form-control'
+                      placeholder='First name'
+                    />
+                  </div>
+                  <div className='col-md-6 mb-4'>
+                    <input
+                      type='text'
+                      className='form-control'
+                      placeholder='Last name'
+                    />
+                  </div>
+                  <div className='col-md-6 mb-4'>
+                    <input
+                      type='tel'
+                      className='form-control'
+                      placeholder='Phone number'
+                    />
+                  </div>
+                  <div className='col-md-6 mb-4'>
+                    <input
+                      type='email'
+                      className='form-control'
+                      placeholder='Email'
+                    />
+                  </div>
+                  <button className='btn btn-primary text-uppercase'>
+                    register your seat
+                  </button>
                 </div>
-                <div className='col-md-6 mb-4'>
-                  <input
-                    type='text'
-                    className='form-control'
-                    placeholder='Last name'
-                  />
-                </div>
-                <div className='col-md-6 mb-4'>
-                  <input
-                    type='tel'
-                    className='form-control'
-                    placeholder='Phone number'
-                  />
-                </div>
-                <div className='col-md-6 mb-4'>
-                  <input
-                    type='email'
-                    className='form-control'
-                    placeholder='Email'
-                  />
-                </div>
-                <button className='btn btn-primary text-uppercase'>
-                  register your seat
-                </button>
-              </div>
-            </form>
+              </form>
+            </div>
           </div>
         </div>
-      </div>
-    </MainContainer>
+      </MainContainer>
+      <Footer />
+    </>
   );
 }
 
