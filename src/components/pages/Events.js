a137cb3f5bda3060129afb8af34e6493400301d2
Vishesh Singh <VisheshSingh16@gmail.com>
2020-05-17 11:01:28 -0500

    redirect by default to mongodb

--- src/components/pages/Events.js
+++ src/components/pages/Events.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Switch, NavLink, Route } from 'react-router-dom';
+import { Switch, NavLink, Route, Redirect } from 'react-router-dom';
 
 import MongoDB from './MongoDB';
 import Express from './Express';
@@ -13,55 +13,44 @@ const Events = () => {
     <EventsContainer>
       <div className='container py-5'>
         <h2 className='text-center mt-5 pt-3 pb-5 text-uppercase'>Events</h2>
-        <NavLink
-          exact
-          to='/events/mongodb'
-          className='badge badge-success mr-3 px-3 py-2'
-        >
-          Mongo DB
-        </NavLink>
-        <NavLink
-          exact
-          to='/events/express'
-          className='badge badge-secondary mr-3 px-3 py-2'
-        >
-          Express
-        </NavLink>
-        <NavLink
-          exact
-          to='/events/react'
-          className='badge badge-primary mr-3 px-3 py-2'
-        >
-          React
-        </NavLink>
-        <NavLink
-          exact
-          to='/events/node'
-          className='badge badge-warning mr-3 px-3 py-2'
-        >
-          NodeJS
-        </NavLink>
+        <div className='my-3'>
+          <NavLink
+            to='/events/mongodb'
+            className='badge badge-success mr-3 px-3 py-2'
+          >
+            Mongo DB
+          </NavLink>
+          <NavLink
+            to='/events/express'
+            className='badge badge-secondary mr-3 px-3 py-2'
+          >
+            Express
+          </NavLink>
+          <NavLink
+            to='/events/react'
+            className='badge badge-primary mr-3 px-3 py-2'
+          >
+            React
+          </NavLink>
+          <NavLink
+            to='/events/node'
+            className='badge badge-warning mr-3 px-3 py-2'
+          >
+            NodeJS
+          </NavLink>
+        </div>
+        <Redirect to='/events/mongodb' />
         <Switch>
           <Route
-            exact
             path='/events/mongodb'
             render={() => <MongoDB data={data} />}
           />
           <Route
-            exact
             path='/events/express'
             render={() => <Express data={data} />}
           />
-          <Route
-            exact
-            path='/events/react'
-            render={() => <ReactJS data={data} />}
-          />
-          <Route
-            exact
-            path='/events/node'
-            render={() => <Node data={data} />}
-          />
+          <Route path='/events/react' render={() => <ReactJS data={data} />} />
+          <Route path='/events/node' render={() => <Node data={data} />} />
         </Switch>
       </div>
     </EventsContainer>
